30d3f9aa3ed4d878403816643a4328b4368db4bd
malisheuski <malisheuski2.18@gmail.com>
2019-07-04 20:15:24 +0300

    add

--- SumDemo.java
+++
@@ -1,7 +0,0 @@
-class SumDemo {
-    public static void main(String args[]) {
-        Summation s1 = new Summation(5);
-        Summation s2 = new Summation(s1);
-        System.out.println("sl.sum: " + s1.sum);
-        System.out.println("s2.suF5555m: " + s2.sum);
-    }}
